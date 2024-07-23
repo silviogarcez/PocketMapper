e699b29e13c5235d5d740e14c83847d3b41d2932
Silvio Jr <silvio__jr@hotmail.com>
2024-07-23 13:48:24 +0100

    fix

---
+++ PocketMapper.Test/Connection.cs
@@ -0,0 +1,26 @@
+﻿using BlazorProject.Domain;
+using Microsoft.Data.SqlClient;
+
+namespace PocketMapper.Test
+{
+    public class PocketTest
+    {
+        private static readonly SqlConnection con = new SqlConnection("Server=tcp:sgarcez.database.windows.net,1433;Initial Catalog=Blazor;Persist Security Info=False;User ID=sgarcez;Password=P@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        public PocketTest()
+        {
+            
+        }
+
+        public static List<T> GetList<T>(string tableName) where T : new()
+        {
+            SqlCommand command = new SqlCommand($"select * from {tableName}", con);
+
+            con.Open();
+            var reader = command.ExecuteReader().ConvertMap<T>();
+            con.Close();
+
+            return reader;
+        }
+    }
+}
---
+++ PocketMapper.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using BlazorProject.Domain;
+using PocketMapper.Test;
+using System.Diagnostics;
+
+var result1 = GetTimer<User>(() => PocketTest.GetList<User>("Users"));
+var result2 = GetTimer<User>(() => PocketTest.GetList<User>("Users"));
+var result3 = GetTimer<Permission>(() => PocketTest.GetList<Permission>("Permission"));
+var result4 = GetTimer<Permission>(() => PocketTest.GetList<Permission>("Permission"));
+string a = ""; 
+
+Tuple<List<T>, string> GetTimer<T>(Func<List<T>> func)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    stopwatch.Start();
+    var ret = func.Invoke();
+    return Tuple.Create(ret,stopwatch.Elapsed.ToString());
+}
